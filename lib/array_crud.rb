8d41ca6d780f80923a7cf6b833026593cee17515
Radele Rose <radele.rose@gmail.com>
2021-01-26 07:04:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,23 +3,31 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  ["Richard", "Robert", "Carmella", "Ronald"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  add_element_to_end_of_array = ["Richard", "Robert", "Carmella", "Ronald"]
+  add_element_to_end_of_array << "Radele"
+  puts add_element_to_end_of_array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
-
+  add_element_to_start_of_array = ["Richard", "Robert", "Carmella", "Ronald"]
+  add_element_to_start_of_array.unshift(Rondora)
+  puts add_element_to_start_of_array.inspect
 end
 
 def remove_element_from_end_of_array(array)
-
+  remove_element_from_end_of_array = ["Richard", "Robert", "Carmella", "Ronald"]
+  Ronald = remove_element_from_end_of_array.pop
+  puts remove_element_from_end_of_array.inspect
 end
 
 def remove_element_from_start_of_array(array)
-
+  remove_element_from_start_of_array = ["Richard", "Robert", "Carmella", "Ronald"]
+  Richard = remove_element_from_start_of_array.shift
+  puts remove_element_from_start_of_array.inspect
 end
 
 def retrieve_element_from_index(array, index_number)
