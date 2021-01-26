93e74fbe8f557763cc633a69a67dee19dfb06d74
Radele Rose <radele.rose@gmail.com>
2021-01-26 07:16:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,25 +9,21 @@ end
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"]
   add_element_to_end_of_array << "arrays!"
-  puts add_element_to_end_of_array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
   add_element_to_start_of_array = ["I", "am", "really", "learning"]
   add_element_to_start_of_array.unshift("wow")
-  puts add_element_to_start_of_array.inspect
 end
 
 def remove_element_from_end_of_array(array)
   remove_element_from_end_of_array = ["I", "am", "really", "learning", "arrays!"]
   remove_element_from_end_of_array.pop
-  puts remove_element_from_end_of_array.inspect
 end
 
 def remove_element_from_start_of_array(array)
   remove_element_from_start_of_array = ["wow", "I", "am", "really", "learning", "arrays!"]
   remove_element_from_start_of_array.shift
-  puts remove_element_from_start_of_array.inspect
 end
 
 def retrieve_element_from_index(array, index_number)
